d3d29b1abbb7001c6da4ecf81c780d533e8b987f
e-liouta <effieliouta@gmail.com>
2025-08-07 09:11:26 +0300

    front page styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,24 +19,72 @@
 	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
-
-<h2>Sign Up</h2>
-<form on:submit|preventDefault={signUp}>
-	<input type="email" bind:value={email} placeholder="Email" required />
-	<input
-		type="password"
-		bind:value={password}
-		placeholder="Password"
-		required
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+		rel="stylesheet"
 	/>
-	<button type="submit">Sign Up</button>
-</form>
+</head>
 
-{#if message}
-	<p>{message}</p>
-{/if}
+<div class="wrapper">
+	<h1>Welcome to FemTech!</h1>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel condimentum
+		lorem. Phasellus pellentesque metus lectus, ut rhoncus diam blandit id.
+	</p>
+
+	<h2>Sign Up</h2>
+	<form on:submit|preventDefault={signUp}>
+		<input type="email" bind:value={email} placeholder="Email" required />
+		<input
+			type="password"
+			bind:value={password}
+			placeholder="Password"
+			required
+		/>
+		<button type="submit">Sign Up</button>
+	</form>
+
+	{#if message}
+		<p>{message}</p>
+	{/if}
+</div>
+
+<style lang="scss">
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		font-family: "Inter", sans-serif;
+	}
+
+	.wrapper {
+		background-image: url("../lib/images/femtech-bg.jpg");
+		background-size: cover;
+		background-position: center;
+		min-height: 100vh;
+		width: 100%;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 2rem;
+		box-sizing: border-box;
+	}
+
+	h1 {
+		color: white;
+		font-size: 5rem;
+		font-weight: 800;
+		margin: 0;
+	}
+
+	p {
+		color: white;
+		font-size: 1.6rem;
+		padding-right: 35rem;
+	}
+</style>
